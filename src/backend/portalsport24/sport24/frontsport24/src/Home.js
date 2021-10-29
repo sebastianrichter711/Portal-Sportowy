c638da761879908e8399e41b6a7eb49fdfdcab12
Sebastian Richter <sebastian.richter@ms.apagroup.pl>
2021-10-29 21:49:18 +0200

    Początki na froncie

---
+++ src/backend/portalsport24/sport24/frontsport24/src/Home.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import './home.css';
+
+
+function Expo() {
+
+   const d = new Date()
+    const weekDay = ['niedziela', 'poniedziałek', 'wtorek', 'środa', 'czwartek', 'piątek', 'sobota'];
+    const months = ['stycznia', 'lutego', 'marca', 'kwietnia', 'maja', 'czerwca', 'lipca', 'sierpnia', 'września', 'października', 'listopada', 'grudnia']
+    const day = weekDay[d.getDay()]
+    const month = months[d.getMonth()]
+    const date = d.getDate()
+    const year = d.getFullYear()
+
+  return (
+  	<>
+    <div className="main">
+          <div className="date">        
+             <h1>{day}, {date} {month} {year}</h1>
+          </div>
+     </div>
+  </>
+  );
+}
+
+export default Expo;
