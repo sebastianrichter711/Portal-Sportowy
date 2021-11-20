77ae21f5075992db5f716360074832002fe2de6a
Sebastian Richter <sebastian.richter@ms.apagroup.pl>
2021-11-20 14:35:11 +0100

    Porządki

--- src/backend/portalsport24/sport24/sport24app/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import *
-
-# Register your models here.
-
-models = [Profile, Section, Article, Comment, Discipline, Game, Season, Match]
-admin.site.register(models)
