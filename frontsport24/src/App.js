7c81feb1d4f3d7be712b5500e0d0f20db615c178
sebix354 <sebarokita733@gmail.com>
2021-12-01 20:59:35 +0100

    Rozwój str gł

--- frontsport24/src/App.js
+++ frontsport24/src/App.js
@@ -1,33 +1,16 @@
 import React from 'react';
-import Expo from './Home';
 import './style.css'
-import fb from './images/fb.png'
-import insta from './images/insta.png'
-import twit from './images/twit.png'
-import logo from './images/logo.png'
+import MainPage from './components/MainPage';
 
-function App() {
-  return (
-    <div className="div">
-      <img className="logo" src={logo} alt="logo"/>
-  <Expo />
-  <a href="https://facebook.com">
-    <img className="fbImg" src={fb} alt="fb"/>
-  </a>
-  <a href="https://twitter.com">
-    <img className="twImg" src={twit} alt="twit"/>
-  </a>
-  <a href="https://instagram.com">
-    <img className="insImg" src={insta} alt="insta"/>
-  </a>
-  <button className="registerButton">Zarejestruj się</button>
-  <button className="loginButton">Zaloguj się</button>
-  <button className="scoresButton">WYNIKI</button>
-  <h1 className="newestNews">Najnowsze</h1>
+class App extends React.Component {
 
-  <h1 className="note">Artykuły w serwisie pochodzą ze strony </h1>
-  </div>
-  );
+  render() {
+    return (
+      <div className="div">
+        <MainPage/>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ frontsport24/src/components/MainPage.js
@@ -0,0 +1,127 @@
+import React from 'react';
+import Expo from './Home';
+import '../style.css'
+import fb from '../images/fb.png'
+import insta from '../images/insta.png'
+import twit from '../images/twit.png'
+import logo from '../images/logo.png'
+
+class MainPage extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      newestMatches: [],
+      newestArticle: [],
+      newestArticles: [],
+      sections: []
+    }
+    this.fetchTasks = this.fetchTasks.bind(this)
+  };
+
+  componentWillMount() {
+    this.fetchTasks()
+  }
+
+  fetchTasks() {
+    console.log("Fetching...")
+
+    fetch('http://127.0.0.1:8000/newest_matches')
+      .then(response => response.json())
+      .then(data =>
+        this.setState({
+          newestMatches: data
+        })
+      )
+
+      fetch('http://127.0.0.1:8000/newest_article')
+      .then(response => response.json())
+      .then(data =>
+        this.setState({
+          newestArticle: data
+        })
+      )
+
+      fetch('http://127.0.0.1:8000/section')
+      .then(response => response.json())
+      .then(data =>
+        this.setState({
+            section: data
+          })
+      )
+      
+
+      fetch('http://127.0.0.1:8000/newest_articles')
+      .then(response => response.json())
+      .then(data =>
+        this.setState({
+          newestArticles: data
+        })
+      )
+    
+
+  }
+
+  render() {
+    var matches = this.state.newestMatches
+    var newArticle = this.state.newestArticle
+    var newArticles = this.state.newestArticles
+    var section = this.state.sections
+    return (
+      <div className="div">
+        <img className="logo" src={logo} alt="logo" />
+        <Expo />
+        <a href="https://facebook.com">
+          <img className="fbImg" src={fb} alt="fb" />
+        </a>
+        <a href="https://twitter.com">
+          <img className="twImg" src={twit} alt="twit" />
+        </a>
+        <a href="https://instagram.com">
+          <img className="insImg" src={insta} alt="insta" />
+        </a>
+        <button className="registerButton">Zarejestruj się</button>
+        <button className="loginButton">Zaloguj się</button>
+        <button className="scoresButton">WYNIKI</button>
+        <div className="newestMatches">
+          {matches.map(function (task, index) {
+            return (
+              <div key={index}>
+                <b className="matchdate">{task.match_date}</b><br/>
+                <b className="team">{task.host}</b>&emsp;<b className="score">{task.host_score}</b><br/>
+                <b className="team">{task.guest}</b>&emsp;<b className="score">{task.guest_score}</b><br/>
+              </div>
+            )
+          })}
+        </div>
+        <div className="newestArticle">
+            <img className="mainImg" src={newArticle.big_title_photo} alt="main_img"/>
+            <p className="mainTitle">{newArticle.title}</p>
+        </div>
+        <div className="sections">
+          {section.map(function (sct, index) {
+            return (
+              <div key={index}>
+                <img className="sectionIcon" src={sct.icon} alt="section_img"/>
+                <p className="sectionName">{sct.name}</p>
+              </div>
+            )
+          })}
+        </div>
+        <h1 className="newestNews">Najnowsze</h1>
+        <div className="newestArticles">
+          {newArticles.map(function (article, index) {
+            return (
+              <div key={index}>
+                <b className="articleDate">{article.date_of_create}</b>&ensp;<b className="articleTitle">{article.title}</b>
+                <p>&nbsp;</p>
+              </div>
+            )
+          })}
+        </div>
+        
+      </div>
+    );
+  }
+}
+
+export default MainPage;
