d94c1426bab483763026d6d9ea5eaf4d9c8fee94
sebix354 <sebarokita733@gmail.com>
2022-01-18 14:48:22 +0100

    NewestMatches- zmiana

--- frontsport24/src/components/NewestMatches.js
+++ frontsport24/src/components/NewestMatches.js
@@ -2,15 +2,10 @@ import React, { useEffect, useState } from 'react';
 import { makeStyles, withStyles } from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
-import CardMedia from '@material-ui/core/CardMedia';
 import Grid from '@material-ui/core/Grid';
 import Typography from '@material-ui/core/Typography';
 import Container from '@material-ui/core/Container';
-import Link from '@material-ui/core/Link';
 import axiosInstance from '../axios';
-import { List, ListItem, ListItemButton } from '@material-ui/core';
-import ListItemText from '@material-ui/core/ListItemText';
-import { Button } from '@material-ui/core';
 
 const useStyles = makeStyles((theme) => ({
     cardMedia: {
